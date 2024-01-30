66ee2557baf0ffa630e8b50dd6c53d4c42466525
SAHIL sanghar <sangharsahil546@gmail.com>
2024-01-30 21:15:15 +0530

    Added functionality to menu bar

--- script.js
+++ script.js
@@ -9,14 +9,16 @@ function openTab(event, tabName) {
     for(let tabContent of tabContents) {
         tabContent.classList.remove("active-tab");
     }
-    const clickedTab = document.getElementById(tabName);
-    if (!clickedTab.classList.contains("active-tab")) {
-        event.currentTarget.classList.add("active-links");
-        clickedTab.classList.add("active-tab");
-    } else {
-        event.currentTarget.classList.remove("active-links");
-    }
-    // event.currentTarget.classList.add("active-links");
-    // document.getElementById(tabName).classList.add("active-tab");
+    event.currentTarget.classList.add("active-links");
+    document.getElementById(tabName).classList.add("active-tab");
+}
 
+let sidemenu = document.getElementById("sidemenu");
+
+function openMenu(){
+    sidemenu.style.right = "0";
+}
+
+function closeMenu(){
+    sidemenu.style.right = "-200px";
 }
