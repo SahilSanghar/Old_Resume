4c9b62308e65734087441cc7c141d3101262a5bc
SAHIL sanghar <sangharsahil546@gmail.com>
2024-02-08 21:42:41 +0530

    Fixed bug in about section

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
-let tabLinks = document.getElementsByClassName("tab-links");
-let tabContents = document.getElementsByClassName("tab-content");
+// Get all tab links and tab contents
+let tabLinks = document.querySelectorAll(".tab-links");
+let tabContents = document.querySelectorAll(".tab-content");
 
+// Add event listeners to each tab link
+tabLinks.forEach((tabLink, index) => {
+  tabLink.addEventListener("click", () => {
+    // Hide all tab contents
+    tabContents.forEach((tabContent) => {
+      tabContent.style.display = "none";
+    });
 
-function openTab(event, tabName) {
-    for(let tabLink of tabLinks) {
-        tabLink.classList.remove("active-links");
-    }
-    for(let tabContent of tabContents) {
-        tabContent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-links");
-    document.getElementById(tabName).classList.add("active-tab");
-}
+    // Remove "active-links" class from all tab links
+    tabLinks.forEach((link) => {
+      link.classList.remove("active-links");
+    });
+
+    // Show the clicked tab content
+    tabContents[index].style.display = "block";
+
+    // Add "active-links" class to the clicked tab link
+    tabLink.classList.add("active-links");
+  });
+});
 
 let sidemenu = document.getElementById("sidemenu");
 
-function openMenu(){
-    sidemenu.style.right = "0";
+function openMenu() {
+  sidemenu.style.right = "0";
 }
 
-function closeMenu(){
-    sidemenu.style.right = "-200px";
+function closeMenu() {
+  sidemenu.style.right = "-200px";
 }
